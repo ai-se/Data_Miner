f209849b29cb977ea806eae6397b446aa5523608
Suvodeep90 <Suvodeep.Majumder90@gmail.com>
2020-01-10 08:23:43 -0500

    check code

--- github/API_V3/new_1 copy.py
+++ github/API_V3/new_1 copy.py
@@ -16,9 +16,7 @@ import numpy as np
 # from threading import Barrier
 # from multiprocessing import Queue
 # from os.path import dirname as up
-import os
-import shlex
-import subprocess as sp
+# import os
 # import platform
 # from commit_guru.cas_manager_v1 import *
 
@@ -26,24 +24,3 @@ print("its working")
 
 df = pd.DataFrame([1,2,3,4,5,6,7])
 df.to_csv('data.csv')
-
-def _os_cmd(cmd, verbose=False):
-        """
-        Run a command on the shell
-
-        Parameters
-        ----------
-        cmd: str
-            A command to run.
-        """
-        cmd = shlex.split(cmd)
-        #print(cmd)
-        with sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.DEVNULL) as p:
-            out, err = p.communicate()
-
-        if verbose:
-            print(out)
-            print(err)
-        return out, err
-
-_os_cmd("git clone https://github.com/docker-java/docker-java.git", verbose=True)
