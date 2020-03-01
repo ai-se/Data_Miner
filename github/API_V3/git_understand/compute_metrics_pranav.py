084838bc234c9d560a3ed30aeec3f5083d072fc1
Pranav Mody <47364762+prmody96@users.noreply.github.com>
2020-02-29 19:51:34 -0500

    FINAL file for mapping

--- github/API_V3/git_understand/compute_metrics_pranav.py
+++ github/API_V3/git_understand/compute_metrics_pranav.py
@@ -66,7 +66,7 @@ class MetricsGetter(object):
         self.repo_lang = repo_lang
         #self.repo_obj = git2repo.git2repo(self.repo_url,self.repo_name)
         self.root_dir = code_path
-        print("root:",self.root_dir)
+        #print("root:",self.root_dir)
         if platform.system() == 'Darwin' or platform.system() == 'Linux':
             self.repo_path = self.root_dir+ '/commit_guru/ingester/CASRepos/git/' + self.repo_name
             self.file_path = up(self.root_dir) + '/data/commit_guru/' + self.repo_name + '.csv'
@@ -74,36 +74,42 @@ class MetricsGetter(object):
             self.und_file = up(self.root_dir) + '/data/understand_files/' + self.repo_name + '_understand.csv'
         else:
             self.repo_path = up(os.getcwd()) + '\\temp_repo\\' + self.repo_name
-            self.file_path = up(os.getcwd()) + '\\data\\commit_guru\\' + self.repo_name + '.pkl'
+            self.file_path = up(os.getcwd()) + '\\data\\commit_guru\\' + self.repo_name + '.csv'
+            self.und_file = up(self.root_dir) + '\\data\\understand_files\\' + self.repo_name + '_understand.csv'
             #self.committed_file = up(os.getcwd()) + '\\data\\committed_files\\' + self.repo_name + '_committed_file.pkl'
         self.buggy_clean_pairs = self.read_commits()
         #self.buggy_clean_pairs = self.buggy_clean_pairs[0:5]
         # Reference current directory, so we can go back after we are done.
-        self.cwd = Path('/tmp/smajumd3/')
+        self.cwd = Path('C:\\Users\\prana\\Desktop\\RA')
         #self.repo = self.clone_repo()
         # Generate path to store udb files
         #self.udb_path = self.cwd.joinpath(".temp", "udb")
-        self.udb_path = self.cwd.joinpath("temp", "udb/"+self.repo_name)
+        self.udb_path = self.cwd.joinpath("temp", "udb\\"+self.repo_name)
 
         # Create a folder to hold the udb files
         if not self.udb_path.is_dir():
             os.makedirs(self.udb_path)
 
     def read_commits(self):
+        print(self.file_path)
         df = pd.read_csv(self.file_path)
         # print(df)
         df = df[df['contains_bug'] == True]
         df = df.reset_index('drop' == True)
         self.commits = []
         commits = []
+        temp_2 = df.shape[0]
+        print(df.shape)
         for i in range(df.shape[0]):
             try:
                 committed_files = []
                 if df.loc[i,'parent_hashes'] == None:
+                    print("Has a parent")
                     continue
                 bug_fixing_commit = df.loc[i,'parent_hashes']
                 bug_existing_commit = df.loc[i,'commit_hash']
-                files_changed = df.loc[i,'fileschanged']
+                files_changed = str(df.loc[i,'fileschanged'])
+
                 #print(files_changed)
                 files_changed = files_changed.split(',')
                 files_changed = list(filter(('CAS_DELIMITER').__ne__, files_changed))
@@ -115,9 +121,10 @@ class MetricsGetter(object):
                     continue
                 for row in files_changed:
                     if language == "Java" or language == "C++" or language == "C":
-                        if len(row.split('src/')) == 1:
-                            continue
-                        committed_files.append(row.split('src/')[1].replace('/','.').rsplit('.',1)[0])
+                        #if len(row.split('src/')) == 1:
+                        #    continue
+                        #committed_files.append(row.split('src/')[1].replace('/','.').rsplit('.',1)[0])
+                        committed_files.append(row.replace('/', '.').rsplit('.', 1)[0])
                     elif language == "Python" :
                         committed_files.append(row['file_path'].replace('/', '.').rsplit('.', 1)[0])
                     elif language == "Fortran" :
@@ -145,7 +152,6 @@ class MetricsGetter(object):
         """
 
         metrics_dataframe = pd.DataFrame()
-        print(len(self.buggy_clean_pairs))
         for i in range(len(self.buggy_clean_pairs)):
             try:
                 buggy_hash = self.buggy_clean_pairs[i][0]
@@ -153,20 +159,40 @@ class MetricsGetter(object):
                 files_changed = self.buggy_clean_pairs[i][2]
                 if len((files_changed)) == 0:
                      continue
-                print(i,self.repo_name,(buggy_hash, clean_hash))
                 # Go the the cloned project path
 
                 #------------BUGGY METRICS-----------------
                 buggy_und_file = self.udb_path.joinpath("{}_{}.csv".format(self.repo_name+buggy_hash, "buggy"))
-                print("Name of buggy und csv file is : ", buggy_und_file)
                 buggy_df = pd.read_csv(buggy_und_file)
-                buggy_df = buggy_df[buggy_df['Kind']=="Class"]
+                buggy_df.Kind.apply(str)
+                #buggy_df = buggy_df[buggy_df['Kind'] == "Class"]
+                #buggy_df = buggy_df[buggy_df['Kind'].isin(class_strs)]
+                buggy_df = buggy_df[buggy_df['Kind'].str.contains("Class", regex = True)]
+                temp_l1 = len(buggy_df)
                 if self.repo_lang == "Python":
                     print("modfiy the Name column")
                 if self.repo_lang == "Fortran":
                     print("modfiy the Name column")
-                new_buggy_df = buggy_df[buggy_df['Name'].isin(list(set(files_changed)))]
-                if new_buggy_df:
+
+                #------ Testing area------------
+                #new_buggy_df = pd.DataFrame()
+                index_list = []
+                name_df = buggy_df['Name'].copy()
+                for index, name in name_df.iteritems():
+                    r = re.compile(str(name))
+                    newList = list(filter(r.search, list(set(files_changed))))
+                    if len(newList)>0:
+                        index_list.append(int(index))
+                        #new_buggy_df = new_buggy_df.append(buggy_df.iloc[index])
+                        #print("new buggy df is : ", new_buggy_df)
+                new_buggy_df = pd.DataFrame()
+                if index_list:
+                    new_buggy_df = buggy_df.loc[index_list]
+
+                #------- Testing area ends--------
+
+                #new_buggy_df = buggy_df[buggy_df['Name'].isin(list(set(files_changed)))]
+                if new_buggy_df.shape[0]!=0:
                     temp = pd.concat([buggy_df, new_buggy_df]).drop_duplicates(keep=False)
                     new_buggy_df["commit_hash"] = buggy_hash
                     new_buggy_df["Bugs"] = 1
@@ -180,17 +206,21 @@ class MetricsGetter(object):
                     metrics_dataframe = metrics_dataframe.append(buggy_df)
 
                 #------------CLEAN METRICS---------------
-                clean_und_file = self.udb_path.joinpath("{}_{}.udb".format(self.repo_name+buggy_hash, "clean"))
-                print("Name of clean und csv file is : ", clean_und_file)
+                clean_und_file = self.udb_path.joinpath("{}_{}.csv".format(self.repo_name+buggy_hash, "clean"))
                 clean_df = pd.read_csv(clean_und_file)
-                clean_df = clean_df[clean_df['Kind'] == "Class"]
+                clean_df = clean_df[clean_df['Kind'].str.contains("Class", regex=True)]
+                #clean_df = clean_df[clean_df['Kind'] == "Class"]
                 clean_df["commit_hash"] = clean_hash
                 clean_df["Bugs"] = 0
                 metrics_dataframe = metrics_dataframe.append(clean_df)
+                print("commit number "+str(i)+" done")
             except Exception as e:
                 print("issue with",buggy_hash)
                 print("Error:",e)
                 continue
         os.chdir(self.root_dir)
+        print(self.root_dir)
+
         metrics_dataframe.to_csv(self.und_file,index=False)
+        print("Created the .csv file")
         return metrics_dataframe
