b6ec75afe170b7763dc539b46d228a048ea6ea62
HuyTu7 <huyqtu7@gmail.com>
2020-03-06 13:02:01 -0500

    completed projects

--- github/API_V3/check_status_1.py
+++ github/API_V3/check_status_1.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from os import path
 
-df = pd.read_csv('Test_projects.csv')
+df = pd.read_csv('Completed_Repos/understand/understand_to_run.csv')
 for i in range(df.shape[0]):
 	project_name = df.loc[i,'repo_name']
 	folder_path = '/tmp/hqtu/temp/udb/' + project_name
 	if path.exists(folder_path):
 		df.loc[i,'done'] = 1
-df.to_csv('all.csv')
+df.to_csv('Completed_Repos/understand/all.csv')
 print(df[df['done']==1].shape)
